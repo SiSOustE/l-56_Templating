524ab8e84cc1e81ba7445fce5dd3bd978266a9b6
SiSOustE <ryzhevsky@gmail.com>
2025-05-26 15:20:22 +0300

    Динамическая web страница → добавлен блок sidebar

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-// main.js
+import Element from "./constructor/Element.js";
 import Wrapper from "./constructor/Wrapper.js";
+
+// Импорт модулей
 import content from "./modules/content.js";
 import footer from "./modules/footer.js";
 import header from "./modules/header.js";
 import sidebar from "./modules/sidebar.js";
 
 // Создаем контейнер для content + sidebar
-const container = new Element("div", "container", [content, sidebar]);
+const container = new Element("div", "container", [
+	content.get(),	// Вызываем get(), чтобы получить HTML Element
+	sidebar.get()		// То же самое для Sidebar
+]);
 
+// Передаем в Wrapper: Header → Container → Footer
 new Wrapper("#root", [header, container, footer]);
