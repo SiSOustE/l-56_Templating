118a6884c6254d63aa5300ea209e851cf4a6145a
SiSOustE <ryzhevsky@gmail.com>
2025-05-29 02:26:37 +0300

    Данные карточек вынесены в модуль data.js

--- modules/content.js
+++ modules/content.js
@@ -1,34 +1,23 @@
+// content.js
 import Element from "../constructor/Element.js";
+import { cardData, smallCardData } from "./data.js";
 
 // Большие карточки
-const cardData = [
-  'Card 1',
-  'Card 2',
-  'Card 3',
-  'Card 4',
-  'Card 5',
-];
-const cards = cardData.map(el => new Element('div', 'card', el).get())
+const cards = cardData.map((el) => new Element("div", "card", el).get());
 
 // Маленькие карточки
-const smallCardData = [
-  'Small 1',
-  'Small 2',
-  'Small 3',
-  'Small 4',
-  'Small 5'
-]
-const smallCards = smallCardData.map((el) => new Element('div', 'small-card',el).get()
-)
+const smallCards = smallCardData.map((el) =>
+  new Element("div", "small-card", el).get()
+);
 
-// Контейнеры для частей
-const leftPanel = new Element('div', 'left-panel', cards).get()
-const rightPanel = new Element('div', 'right-panel', smallCards).get()
+// Контейнеры
+const leftPanel = new Element("div", "left-panel", cards).get();
+const rightPanel = new Element("div", "right-panel", smallCards).get();
 
 // Общий контейнер
-const contentContainer = new Element('div', 
-  'content-container',
-  [leftPanel, rightPanel]
-)
+const contentContainer = new Element("div", "content-container", [
+  leftPanel,
+  rightPanel,
+]);
 
 export default contentContainer;
---
+++ modules/data.js
@@ -0,0 +1,7 @@
+// Данные карточек
+
+const cardData = ['Card 1', 'Card 2', 'Card 3', 'Card 4', 'Card 5'];
+
+const smallCardData = ['Small 1', 'Small 2', 'Small 3', 'Small 4', 'Small 5'];
+
+export { cardData, smallCardData };
